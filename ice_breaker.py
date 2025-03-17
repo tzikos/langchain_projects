8908759ceda076b99f59efb102a706ec3bfcde5b
tzikos <s242798@dtu.dk>
2025-03-17 17:21:24 +0100

    adds personal links

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
 
 from third_parties.linkedin import scrape_linkedin_profile
 
@@ -13,17 +15,20 @@ if __name__ == "__main__":
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
+    Make sure to make the person appealing to recruiters
     """
 
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOllama(model='mistral')
 
-    chain = summary_prompt_template | llm
+    chain = summary_prompt_template | llm | StrOutputParser()
     linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+        linkedin_profile_url="https://www.linkedin.com/in/dimitris-papantzikos/",
+        mock=True
     )
     res = chain.invoke(input={"information": linkedin_data})
 
--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -4,20 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
-    """scrape information from LinkedIn profiles,
-    Manually scrape the information from the LinkedIn profile"""
-
     if mock:
-        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/32f3c85b9513994c572613f2c8b376b633bfc43f/eden-marco-scrapin.json"
+        linkedin_profile_url = "https://gist.githubusercontent.com/tzikos/578530d724210cbb26f13b4cc212e123/raw/61d6c36b79051aefb1fd1283449ba49079295384/tzikos-scrapin.json"
         response = requests.get(
             linkedin_profile_url,
             timeout=10,
         )
     else:
-        # ENROLL WITH COUPON CODE: EDENMARCO
-        # For 20% Discount on all pricing
         api_endpoint = "https://api.scrapin.io/enrichment/profile"
         params = {
             "apikey": os.environ["SCRAPIN_API_KEY"],
@@ -30,10 +24,11 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
         )
 
     data = response.json().get("person")
+
     data = {
         k: v
         for k, v in data.items()
-        if v not in ([], "", "", None) and k not in ["certifications"]
+        if v not in ({}, [], "", "", None)
     }
 
     return data
@@ -42,6 +37,7 @@ def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
 if __name__ == "__main__":
     print(
         scrape_linkedin_profile(
-            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
-        ),
+            linkedin_profile_url='https://www.linkedin.com/in/dimitris-papantzikos/',
+            mock=True
+        )
     )
